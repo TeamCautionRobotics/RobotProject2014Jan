78a2fcc4b31146f42d23a849c15019332a3c2884
docprofsky <schuyler.st.leger@gmail.com>
2014-02-04 08:57:34 -0700

    Added scale and offset to the motors
    Added scaling and offset functionality to the motors.  Added comments
    to the code.

--- src/edu/wpi/first/wpilibj/templates/RobotMain.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMain.java
@@ -24,6 +24,15 @@ public class RobotMain extends SimpleRobot {
     Servo servoTest;
     DriverStation driverStation;
 
+    final int frontLeft = 0;
+    final int backLeft = 1;
+    final int backRight = 2;
+    final int frontRight = 3;
+
+    double[] motorValues = new double[4];
+    double[] offsetTrim = new double[4];
+    double[] scaleTrim = new double[4];
+
     Talon fl;
     Talon bl;
     Talon fr;
@@ -45,18 +54,19 @@ public class RobotMain extends SimpleRobot {
 
         servoTest = new Servo(5);
 
-        fl = new Talon(1);
+        fl = new Talon(1);  //create the talons for each of the four wheels
         bl = new Talon(2);
         br = new Talon(3);
         fr = new Talon(4);
+
         compressor.start();
-        chassis = new RobotDrive(fl, bl, fr, br);
-        chassis.setExpiration(2);// since this is a double it's probrably seconds not miliseconds
-        chassis.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        chassis = new RobotDrive(fl, bl, fr, br);   //create the chassis with the motor drivers
+        chassis.setExpiration(2);   // since this is a double it's probrably seconds not miliseconds
+        chassis.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);   //invert the two motors so the robot drives correctly
         chassis.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
     }
 
-    public void autonomous() {
+    public void autonomous() {  //this method is called once when the robot is autonomous mode
         visionProcessing.autonomousInit();
         BinaryImage filteredImage;
 
@@ -64,38 +74,49 @@ public class RobotMain extends SimpleRobot {
             driveNowhere();
 
             try {
-                filteredImage = visionProcessing.filterImage(camera.getImage());
-                visionProcessing.autonomousUpdate(filteredImage);
+                filteredImage = visionProcessing.filterImage(camera.getImage());    //get an image and filter for vision targets
+                visionProcessing.autonomousUpdate(filteredImage);   //process the filtered image and look for any potential targets
             } catch (Exception e) {
-                
             }
 
             SmartDashboard.putBoolean("Target Hot", visionProcessing.target.Hot);
-            Timer.delay(.01);
+            Timer.delay(0.01);
         }
     }
 
-    public void operatorControl() {
+    public void operatorControl() { //this method is called once when the robot is teleoperated mode
         chassis.setSafetyEnabled(true);
         SmartDashboard.putString("Alliance", driverStation.getAlliance().name);
         while (this.isOperatorControl() && this.isEnabled()) {
-            SmartDashboard.putNumber("Mecanum X", getMecX());
+            SmartDashboard.putNumber("Mecanum X", getMecX());   //put the different motor and joystick values on the dashboard for debugging
             SmartDashboard.putNumber("Mecanum Y", getMecY());
             SmartDashboard.putNumber("Mecanum Rotation", getMecRot());
             SmartDashboard.putNumber("Front Left", fl.getSpeed());
             SmartDashboard.putNumber("Front Right", fr.getSpeed());
             SmartDashboard.putNumber("Back Left", bl.getSpeed());
             SmartDashboard.putNumber("Back Right", br.getSpeed());
-
-            mecanumDrive(getMecX(), getMecY(), getMecRot());
+            motorValues = mecanumDrive(getMecX(), getMecY(), getMecRot());  //
             //chassis.mecanumDrive_Cartesian(getMecX(), getMecY(), getMecRot(), 0);
+
+            offsetTrim[frontLeft] = SmartDashboard.getNumber("Front Left Offset");  //get the offsets for the motors
+            offsetTrim[frontRight] = SmartDashboard.getNumber("Front Right Offset");
+            offsetTrim[backLeft] = SmartDashboard.getNumber("Back Left Offset");
+            offsetTrim[backRight] = SmartDashboard.getNumber("Back Right Offset");
+
+            scaleTrim[frontLeft] = SmartDashboard.getNumber("Front Left Scale");    //get the scaling for the motors
+            scaleTrim[frontRight] = SmartDashboard.getNumber("Front Right Scale");
+            scaleTrim[backLeft] = SmartDashboard.getNumber("Back Left Scale");
+            scaleTrim[backRight] = SmartDashboard.getNumber("Back Right Scale");
+
+            motorValues = trimValues(motorValues, scaleTrim, offsetTrim);   //scale and offset the motors
+            driveMotors(motorValues);   //set the motors to the scaled and offset values
 
             if (rightStick.getRawButton(3)) { //up
                 servoTest.setAngle(-360);
             } else if (rightStick.getRawButton(2)) { //down
                 servoTest.setAngle(360);
             }
-            Timer.delay(.01);
+            Timer.delay(0.01);  //do not run the loop to fast
         }
 
     }
@@ -104,8 +125,8 @@ public class RobotMain extends SimpleRobot {
 
     }
 
-    public void test() {
-        
+    public void test() {    //this method is called once when the robot is test mode
+
         visionProcessing.autonomousInit();
         BinaryImage filteredImage;
 
@@ -117,34 +138,34 @@ public class RobotMain extends SimpleRobot {
         }
         SmartDashboard.putBoolean("Target Hot", visionProcessing.target.Hot);
 
-        while (this.isTest() && this.isEnabled()) {
+        while (this.isTest() && this.isEnabled()) { //keep the robot from returning errors
             driveNowhere();
             Timer.delay(0.1);
         }
-        
+
     }
 
-    private double getMecX() {
+    private double getMecX() {  //get joystick values
         return deadZone(rightStick.getAxis(Joystick.AxisType.kX));
     }
 
-    private double getMecY() {
+    private double getMecY() {  //get joystick values
         return deadZone(rightStick.getAxis(Joystick.AxisType.kY));
     }
 
-    private double getMecRot() {
+    private double getMecRot() {    //get joystick values
         return deadZone(leftStick.getAxis(Joystick.AxisType.kX));
     }
 
-    private double deadZone(double value) {
+    private double deadZone(double value) { //apply a dead zone to a value
         return (abs(value) < .1) ? 0 : value;
     }
 
-    private double abs(double value) {
+    private double abs(double value) {  //absoulte value
         return value < 0 ? -value : value;
     }
 
-    private void mecanumDrive(double x, double y, double r) {
+    private double[] mecanumDrive(double x, double y, double r) {   // find the values for the motors based on x, y and rotation values
         y = -y;
 
         double frn = 0;
@@ -152,23 +173,42 @@ public class RobotMain extends SimpleRobot {
         double brn = 0;
         double bln = 0;
 
-        frn *= 1;
-        fln *= 1;
-        brn *= 1;
-        bln *= 1;
-
         fln = y + x + r;
         frn = y - x - r;
         bln = y - x + r;
         brn = y + x - r;
 
-        fr.set(-maxAt1(frn));
-        fl.set(maxAt1(fln));
-        br.set(-maxAt1(brn));
-        bl.set(maxAt1(bln));
+        double[] values = new double[4];
+
+        values[frontRight] = frn;
+        values[frontLeft] = fln;
+        values[backRight] = brn;
+        values[backLeft] = bln;
+        return values;
     }
 
-    private double maxAt1(double n) {
+    private double[] trimValues(double[] orignalValues, double[] scale, double[] offset) {  //apply offset and scaling to the values
+        double[] trimedValues = new double[4];
+
+        for (int i = 0; i < 4; i++) {
+            trimedValues[i] = orignalValues[i] * scale[i];
+        }
+
+        for (int i = 0; i < 4; i++) {
+            trimedValues[i] = orignalValues[i] + offset[i];
+        }
+
+        return trimedValues;
+    }
+
+    private void driveMotors(double[] motorValues) {    //assign the values to the motors
+        fl.set(maxAt1(motorValues[frontLeft]));
+        bl.set(maxAt1(motorValues[backLeft]));
+        br.set(maxAt1(motorValues[backRight]));
+        fr.set(maxAt1(motorValues[frontRight]));
+    }
+
+    private double maxAt1(double n) {   //make the input value between 1 and -1
         return n < -1 ? -1 : (n > 1 ? 1 : n);
     }
 
